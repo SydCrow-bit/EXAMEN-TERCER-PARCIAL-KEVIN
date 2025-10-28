22eb8fe4a160b72bce494c18eb0213f681ae7318
SydCrow-bit <kevincm400@gmail.com>
2025-10-27 20:11:11 -0400

    feat(api): crear controlador y rutas API REST funcional GET/POST empleados

---
+++ app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Empleado;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class EmpleadoController extends Controller
+{
+    public function index()
+    {
+        $empleados = Empleado::all();
+        return response()->json($empleados);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nombre' => 'required|string|max:255',
+            'apellido' => 'required|string|max:255',
+            'correo' => 'required|email|unique:empleados,correo',
+            'salario' => 'required|numeric|min:0'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $empleado = Empleado::create($request->all());
+
+        return response()->json($empleado, 201);
+    }
+}
